ff4ed653f7703ace15ccb5522519f91c558e24be
Ulysses <ulysses.salinas88@gmail.com>
2024-06-10 15:06:58 -0500

    Add for of loop in printLogHandler function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -152,7 +152,9 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  console.log(battleLog);
+  for (const logEntry of battleLog) {
+    console.log(logEntry);
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
